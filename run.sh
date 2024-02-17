26019bee63c211be3dbe97b9d44d6bd14410b595
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2024-02-17 10:35:09 +0530

    maint: misc presentation improvements; preview via run.sh

--- run.sh
+++ run.sh
@@ -21,23 +21,23 @@ echo -e "${BLUE}==> Build & Generate${RESET}"
 pdm run python ./src/hymnal/main.py &&
   pnpm build
 
-# # preview
-# pnpm preview
+# preview
+pnpm preview
 
-# deploy
-commands=("docker" "podman")
-for cmd in "${commands[@]}"; do
-  if command_exists "$cmd"; then
-    echo -e "\n${BLUE}==> Deploy with '$cmd'${RESET}\n"
-    $cmd-compose -p hymnal -f ./compose.yaml down
-    $cmd-compose -p hymnal -f ./compose.yaml up -d
-    echo -e "\n${GREEN}==> Open http://[::1]:8080 in your browser${RESET}"
-    exit $?
-  else
-    echo -e "\n${YELLOW}==> Warning: Command '$cmd' not found${RESET}"
-  fi
-done
-echo -e "\n${RED}==> Error: Deploymnet failed${RESET}"
-exit 1
+# # deploy
+# commands=("docker" "podman")
+# for cmd in "${commands[@]}"; do
+#   if command_exists "$cmd"; then
+#     echo -e "\n${BLUE}==> Deploy with '$cmd'${RESET}\n"
+#     $cmd-compose -p hymnal -f ./compose.yaml down
+#     $cmd-compose -p hymnal -f ./compose.yaml up -d
+#     echo -e "\n${GREEN}==> Open http://[::1]:8080 in your browser${RESET}"
+#     exit $?
+#   else
+#     echo -e "\n${YELLOW}==> Warning: Command '$cmd' not found${RESET}"
+#   fi
+# done
+# echo -e "\n${RED}==> Error: Deploymnet failed${RESET}"
+# exit 1
 
 set +e
