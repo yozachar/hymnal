9c5ed1932ac44bdd66ef5e3644b9cc9f17f8b154
yozachar <dev@void.null>
2024-04-16 18:22:38 +0530

    feat: adds json to html converter using jinja2

--- generator/converter.py
+++ generator/converter.py
@@ -5,11 +5,12 @@
 
 # standard
 from dataclasses import dataclass, field, asdict
+from json import dump, load
 from pathlib import Path
 from typing import List
-from json import dump
 
 # external
+from jinja2 import Environment, PackageLoader, select_autoescape
 from bs4 import BeautifulSoup
 
 
@@ -19,7 +20,7 @@ class Stanza:
     Store hymn stanza
     """
 
-    stanza_number: int
+    stanza_number: int = -1
     lines: List[str] = field(default_factory=list[str])
 
 
@@ -29,28 +30,28 @@ class Hymn:
     Store hymn
     """
 
-    hymn_number: int
-    starts_on: str
+    hymn_number: int = -1
+    starts_on: str = ""
     chorus: List[str] = field(default_factory=list[str])
     stanzas: List[Stanza] = field(default_factory=list[Stanza])
 
 
-def song_parser(hymn_file: Path) -> Hymn:
-    """
-    Parse songs from reveal.js HTML files
-    """
-
+def _html_song_parser(hymn_file: Path) -> Hymn:
+    """Parse songs from reveal.js HTML files"""
     with open(file=hymn_file, mode="rt", encoding="utf-8") as _html_file:
         _html_content = _html_file.read()
 
     _soup = BeautifulSoup(_html_content, "lxml")
-    hymn = Hymn(
-        hymn_number=int(_hyn.text) if (_hyn := _soup.select_one("h1.hymn-no")) else -1,
-        starts_on=(
-            "stanza"
-            if ('<section id="stanza-1" class="s-hymn">' in _html_content)
-            else "chorus"
-        ),
+
+    hymn = Hymn()
+
+    if _hyn := _soup.select_one("h1.hymn-no"):
+        hymn.hymn_number = int(_hyn.text)
+
+    hymn.starts_on = (
+        "stanza-1"
+        if '<section id="stanza-1" class="s-hymn">' in _html_content
+        else "chorus"
     )
 
     for _stanza_section in _soup.select('section[id^="stanza-"]'):
@@ -62,7 +63,6 @@ def song_parser(hymn_file: Path) -> Hymn:
         _lines = [line.text.strip() for line in _stanza_section.select("h3")]
         _lines.extend([line.text.strip() for line in _stanza_section.select("h4")])
         hymn.stanzas.append(Stanza(stanza_number=_stanza_number, lines=_lines))
-
     if (chorus_section := _soup.select_one('section[id^="chorus"]')) and (
         _verses := chorus_section.select("em")
     ):
@@ -72,17 +72,105 @@ def song_parser(hymn_file: Path) -> Hymn:
     return hymn
 
 
-ag_hymnal_html = Path(__file__).parent.parent / "static/hymnal/ag"
-ag_hymnal_json = Path(__file__).parent / "ag/json"
-ag_hymnal_json.mkdir(parents=True, exist_ok=True)
+def html_to_json():
+    """Convert HTML to JSON"""
+    for idx in range(1, 2001):
+        hymn_path = ag_hymnal_html / f"{str(idx).zfill(4)}.html"
+        if not hymn_path.exists() or not hymn_path.is_file():
+            continue
+        with open(
+            file=ag_hymnal_json / f"{str(idx).zfill(4)}.json",
+            mode="wt",
+            encoding="utf-8",
+        ) as ag_f:
+            dump(obj=asdict(_html_song_parser(hymn_path)), fp=ag_f, ensure_ascii=False)
 
-for idx in range(1, 2001):
-    hymn_path = ag_hymnal_html / f"{str(idx).zfill(4)}.html"
-    if not hymn_path.exists() or not hymn_path.is_file():
-        continue
-    with open(
-        file=ag_hymnal_json / f"{str(idx).zfill(4)}.json", mode="wt", encoding="utf-8"
-    ) as ag_f:
-        dump(obj=asdict(song_parser(hymn_path)), fp=ag_f, ensure_ascii=False)
 
 # pprint(song_parser(ag_hymnal_html / "0142.html"))
+
+
+############################## JSON-HTML ##############################
+
+
+def _json_song_reader(hn_f: Path):
+    """Parse songs from JSON files"""
+    with open(file=hn_f, mode="rb") as ag_f:
+        # Dict[
+        #     str, int | str | List[str] | List[Dict[str, int | List[str]]]
+        # ]
+        return load(ag_f)
+
+
+def json_to_html():
+    """Convert JSON to HTML"""
+
+    # hymn_path = ag_hymnal_json / "0206.json"  # sac
+    # hymn_path = ag_hymnal_json / "1114.json" # saswc
+    hymn_path = ag_hymnal_json / "0366.json" # sasnc
+
+    hymn = Hymn(**_json_song_reader(hymn_path))
+
+    jj_env = Environment(
+        loader=PackageLoader("converter"),
+        autoescape=select_autoescape(),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+
+    if hymn.chorus:
+        if hymn.starts_on == "chorus":
+            print(
+                jj_env.get_template("sac.html.j2").render(
+                    hymn_number=hymn.hymn_number,
+                    # main section
+                    stanza_list=hymn.stanzas,
+                    ms_tag="<h3>",
+                    me_tag="</h3>",
+                    # side section
+                    chorus=hymn.chorus,
+                    ss_tag="<h3><em>",
+                    se_tag="</em></h3>",
+                )
+            )
+            return
+        print(
+            jj_env.get_template("saswc.html.j2").render(
+                hymn_number=hymn.hymn_number,
+                # main section
+                stanza_1=hymn.stanzas[0],
+                stanza_list=hymn.stanzas[1:],
+                ms_tag="<h3>",
+                me_tag="</h3>",
+                # side section
+                chorus=hymn.chorus,
+                ss_tag="<h3><em>",
+                se_tag="</em></h3>",
+            )
+        )
+        return
+    print(
+        jj_env.get_template("sasnc.html.j2").render(
+            hymn_number=hymn.hymn_number,
+            # main section
+            stanza_list=hymn.stanzas[1:],
+            ms_tag="<h3>",
+            me_tag="</h3>",
+            # side section
+            stanza_1=hymn.stanzas[0],
+            ss_tag="<h3>",
+            se_tag="</h3>",
+        )
+    )
+
+
+pgm_root = Path(__file__).parent.parent
+ag_hymnal_html = pgm_root / "static/hymnal/ag"
+ag_hymnal_json = pgm_root / "generator/ag/json"
+ag_hymnal_json.mkdir(parents=True, exist_ok=True)
+# template_file = pgm_root / "generator/templates/hymn.html.j2"
+
+
+if __name__ == "__main__":
+    html_to_json()
+    # ref: https://github.com/pallets/jinja/issues/1239#issuecomment-645231227
+    json_to_html()
