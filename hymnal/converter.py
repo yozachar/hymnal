1a8a5d57e3817c947903fd1e9df4f7e3d251a620
yozachar <dev@void.null>
2024-04-16 18:22:38 +0530

    feat: updates converter and related files
    - improves `converter` module
    - adds fonts and styles as static
    - fix path in `jinja2` templates
    - project specific path in `.gitignore`

--- hymnal/converter.py
+++ hymnal/converter.py
@@ -6,6 +6,7 @@
 # standard
 from dataclasses import dataclass, field, asdict
 from json import dump, load
+from shutil import copytree
 from pathlib import Path
 from typing import List
 
@@ -36,7 +37,7 @@ class Hymn:
     stanzas: List[Stanza] = field(default_factory=list[Stanza])
 
 
-def _html_song_parser(hymn_file: Path) -> Hymn:
+def _html_song_parser(hymn_file: Path):
     """Parse songs from reveal.js HTML files"""
     with open(file=hymn_file, mode="rt", encoding="utf-8") as _html_file:
         _html_content = _html_file.read()
@@ -72,14 +73,14 @@ def _html_song_parser(hymn_file: Path) -> Hymn:
     return hymn
 
 
-def html_to_json():
+def html_to_json(source: Path, destination: Path):
     """Convert HTML to JSON"""
     for idx in range(1, 2001):
-        hymn_path = ag_hymnal_html / f"{str(idx).zfill(4)}.html"
+        hymn_path = source / f"{str(idx).zfill(4)}.html"
         if not hymn_path.exists() or not hymn_path.is_file():
             continue
         with open(
-            file=ag_hymnal_json / f"{str(idx).zfill(4)}.json",
+            file=destination / f"{str(idx).zfill(4)}.json",
             mode="wt",
             encoding="utf-8",
         ) as ag_f:
@@ -89,20 +90,10 @@ def html_to_json():
 ############################## JSON-HTML ##############################
 
 
-def _json_song_reader(hn_f: Path):
+def _json_song_parser(hymn_path: Path):
     """Parse songs from JSON files"""
-    with open(file=hn_f, mode="rb") as ag_f:
-        return load(ag_f)
-
-
-def json_to_html():
-    """Convert JSON to HTML"""
-
-    hymn_path = ag_hymnal_json / "0206.json"  # SAC
-    # hymn_path = ag_hymnal_json / "1114.json" # SASWC
-    # hymn_path = ag_hymnal_json / "0366.json"  # SASNC
-
-    hymn = Hymn(**_json_song_reader(hymn_path))
+    with open(file=hymn_path, mode="rb") as ag_f:
+        hymn = Hymn(**load(ag_f))
 
     jj_env = Environment(
         loader=PackageLoader("converter"),
@@ -110,6 +101,9 @@ def json_to_html():
         trim_blocks=True,
         lstrip_blocks=True,
     )
+
+    if hymn.hymn_number == -1 or len(hymn.stanzas) < 1:
+        return f"<html>\n<title>BAD FILE</title>\n<body>\n<p>{hymn_path}</p>\n<body>\n</html>"
 
     if hymn.chorus:
         chorus = hymn.chorus
@@ -129,30 +123,57 @@ def json_to_html():
         stz_list = hymn.stanzas[1:]
         stanza_1 = hymn.stanzas[0]
 
-    print(
-        jj_env.get_template(template_file).render(
-            hymn_number=hymn.hymn_number,
-            # main section
-            stanza_list=stz_list,
-            ms_tag="<h3>",
-            me_tag="</h3>",
-            # side section
-            chorus=chorus,
-            ss_tag=ss_tag,
-            se_tag=se_tag,
-            # other
-            stanza_1=stanza_1,
-        )
+    return jj_env.get_template(template_file).render(
+        hymn_number=hymn.hymn_number,
+        # main section
+        stanza_list=stz_list,
+        ms_tag="<h3>",
+        me_tag="</h3>",
+        # side section
+        chorus=chorus,
+        ss_tag=ss_tag,
+        se_tag=se_tag,
+        # other
+        stanza_1=stanza_1,
+        rel_path="..",
     )
 
 
-pgm_root = Path(__file__).parent.parent
-ag_hymnal_html = pgm_root / "static/hymnal/ag"
-ag_hymnal_json = pgm_root / "generator/ag/json"
-ag_hymnal_json.mkdir(parents=True, exist_ok=True)
+def json_to_html(source: Path, destination: Path):
+    """Convert JSON to HTML"""
+    for idx in range(1, 2001):
+        hymn_path = source / f"{str(idx).zfill(4)}.json"
+        if not hymn_path.exists() or not hymn_path.is_file():
+            continue
+        with open(
+            file=destination / f"{str(idx).zfill(4)}.html",
+            mode="wt",
+            encoding="utf-8",
+        ) as ag_f:
+            ag_f.write(_json_song_parser(hymn_path))
 
+
+pgm_root = Path(__file__).parent.parent
+# temporary_source_ag_hymnal_html = pgm_root / "hymnal/temp/ag/html"
+ag_hymnal_json = pgm_root / "hymnal/ag"
+# ag_hymnal_json.mkdir(parents=True, exist_ok=True)
+output_ag_hymnal_html = pgm_root / "dist/ag"
+output_ag_hymnal_html.mkdir(parents=True, exist_ok=True)
 
 if __name__ == "__main__":
-    # html_to_json()
-    # ref: https://github.com/pallets/jinja/issues/1239#issuecomment-645231227
-    json_to_html()
+    # html_to_json(temporary_source_ag_hymnal_html, ag_hymnal_json)
+    json_to_html(ag_hymnal_json, output_ag_hymnal_html)
+    copytree(
+        src=pgm_root / "hymnal/ag/styles",
+        dst=pgm_root / "dist/ag/styles",
+        dirs_exist_ok=True,
+    )
+    copytree(
+        src=pgm_root / "hymnal/ag/fonts",
+        dst=pgm_root / "dist/ag/fonts",
+        dirs_exist_ok=True,
+    )
+    (pgm_root / "dist/lib/reveal.js").mkdir(parents=True, exist_ok=True)
+
+# reason for using __name__ == '__main__'
+# ref: https://github.com/pallets/jinja/issues/1239#issuecomment-645231227
